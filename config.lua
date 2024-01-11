7209407e60fe92ad70965b965315db1f75d3655f
Karobben <591465908@qq.com>
2024-01-11 01:31:07 -0600

    added spelling check

--- config.lua
+++ config.lua
@@ -6,17 +6,49 @@
 
 -- fot reple the codes
 lvim.plugins = {
-  { "jpalardy/vim-slime" },
+  { 'jalvesaq/Nvim-R', branch = 'stable' },
+  { 'ncm2/ncm2'},
+  { 'roxma/nvim-yarp'},
+  { 'gaalcaras/ncm-R'},
+  { 'preservim/nerdtree'},
+  { 'Raimondi/delimitMate'},
+  { 'patstockwell/vim-monokai-tasty'},
+  { 'itchyny/lightline.vim'},
+  {
+   "jpalardy/vim-slime",
+   config = function()
+     -- vim.cmd([[
+     -- let g:slime_default_config = { "sessionname": "NVIM", "windowname": "0"}
+     -- let g:slime_dont_ask_default = 1
+     -- let g:slime_cell_delimiter = "#%%"
+     -- nmap <leader>a <Plug>SlimeSendCell
+     -- ]])
+   end
+  }
 }
 
-vim.cmd [[let g:slime_target = "tmux"]]
+lvim.builtin.which_key.mappings["r"] = {
+    name = " Run",
+    p = {
+      "<cmd>TermExec open=1<CR><cmd>ToggleTermSendCurrentLine<CR>", --<cmd>ToggleTerm<CR>",
+      "Python REPL",
+    },
+    -- add repl for other langs
+}
+-- vim.cmd [[let g:slime_target = "toggleterm.nvim"]]
+-- vim.cmd [[let g:slime_target = "tmux"]]
+-- " NERD Tree
+vim.cmd [[map nn :NERDTreeToggle]]
+-- NCM2
+vim.cmd [[autocmd BufEnter * call ncm2#enable_for_buffer() ]] -- To enable ncm2 for all buffers.
+vim.cmd [[set completeopt=noinsert,menuone,noselect ]] -- help Ncm2PopupOpen for more information.
 
+-- Spell check
+vim.cmd [[ set spell spelllang=en_us ]]
 -- other configuration
 -- text wrap
 vim.wo.wrap = true
-
 -- code fold
-
 vim.opt.foldmethod = "expr" -- default is "normal"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- default is ""
 vim.opt.foldenable = true -- if this option is true and fold method option is other than normal, every time a document is opened everything will be folded.
